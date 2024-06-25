745cf7f1502d43393b2d1944e3e697c996d69de6
Rafael Oviedo <rafaeloviedo@Rafaels-Air.fibertel.com.ar>
2024-06-25 17:56:33 -0300

    update: adding urls list table

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import { Route, Routes, Navigate } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import NavBar from "./components/NavBar/NavBar";
 import LandingPage from './views/LandingPage/LandingPage';
 import About from './views/About/About';
 import Error404 from './views/Error404/Error404';
 import style from './App.module.css';
-import { useSelector } from 'react-redux';
+// import { useSelector } from 'react-redux';
 
 function App() {
-  const currentUser = useSelector((state) => state.auth.user);
+  // const currentUser = useSelector((state) => state.auth.user);
 
   return (
     <div className={style.appContainer}>
@@ -15,7 +15,7 @@ function App() {
       <Routes>
         <Route path='/' element={<LandingPage />} />
         <Route path='/about' element={<About />} />
-        
+
         <Route path='/*' element={<Error404 />} />
       </Routes>
     </div>
--- src/views/LandingPage/LandingPage.js
+++ src/views/LandingPage/LandingPage.js
@@ -1,15 +1,49 @@
-import { useNavigate } from 'react-router-dom';
+import { useCallback, useEffect, useState } from 'react';
 import style from './LandingPage.module.css';
+import axios from 'axios';
+
+import { ProgressSpinner } from 'primereact/progressspinner';
 
 function LandingPage() {
-  const navigate = useNavigate();
+  const [urlsList, setUrlsList] = useState([]);
+  const [urlInput, setUrlInput] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
-  const handleGroupInputChange = (event) => {
-    console.log('VALUE', event.target.value);
+  const handleUrlInputChange = (event) => {
+    setUrlInput(event.target.value)
   }
-  const searchGroup = () => {
-    console.log('searching...')
+
+  const addUrl = async () => {
+    setIsLoading(true);
+
+    try {
+      await axios.post('/url/shorten', { longUrl: urlInput });
+      setUrlInput('');
+      setIsLoading(false);
+    } 
+    catch (error) {
+      setIsLoading(false);
+      throw new Error(error);  
+    }
   }
+
+  const getAllUrls = useCallback(async () => {
+    setIsLoading(true);
+
+    try {
+      const response = await axios.get('/url');
+      setUrlsList(response.data.urls);
+      setIsLoading(false);
+    } 
+    catch (error) {
+      setIsLoading(false);
+      throw new Error(error);  
+    }
+  }, []);
+
+  useEffect(() => {
+    getAllUrls();
+  }, [getAllUrls])
   
   return (
     <div className={style.landingPageView}>
@@ -21,14 +55,41 @@ function LandingPage() {
         <div className={style.inputButtonContainer}>
           <label style={ { color: '#fff', fontSize: '15px' } }>Add an URL</label>
           <div className={style.inputContainer}>
-            <input onChange={handleGroupInputChange} className={style.addUrlInput} type='text' placeholder='Long url...' />
-            <button onClick={searchGroup} className={style.addUrlButton}>ADD</button>
+            <input onChange={handleUrlInputChange} className={style.addUrlInput} value={urlInput} type='text' placeholder='Long url...' />
+            <button onClick={addUrl} className={style.addUrlButton}>ADD</button>
           </div>
         </div>
       </div>
 
       <div className={style.urlsList}>
-        Url's List
+        <div className={style.listHeader}>
+          <div className={style.longUrlHeader}>Long Url</div>
+          <div className={style.shortUrlHeader}>Short Url</div>
+          <div className={style.clicksHeader}>Clicks</div>
+        </div>
+
+        {
+          isLoading ? (
+            <ProgressSpinner style={{width: '50px', height: '50px', marginTop: '10px'}} strokeWidth="4" />
+          ) : (
+            <div className={style.urlsListContainer}>
+              {
+                !urlsList?.length ? (
+                  <div className={style.noUrlsDisplayed}>No URL's have been added yet</div>
+                ) : (
+                urlsList && urlsList.map((url) => (
+                  <div key={url._id} className={style.urlRow}>
+                    <div className={style.longUrlColumn}>{ url.longUrl }</div>
+                    <div className={style.shortUrlColumn}>
+                      <a className={style.urlLink} href={url.shortUrl}>{ url.shortUrl }</a>
+                    </div>
+                    <div className={style.clicksColumn}>{ url.clicks }</div>
+                  </div>
+                ))
+              )}
+            </div>
+          )
+        }
       </div>
     </div>
   )
